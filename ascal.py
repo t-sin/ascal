115a578687490db3a3ac4ca03cc0437df89095f2
t-sin <shinichi.tanaka45@gmail.com>
2017-07-29 19:56:35 +0900

    Adds specification

--- ascal.py
+++ ascal.py
@@ -1,4 +1,22 @@
 """
+Ascal is an associative calculator.
+It's just a toy calculator program with simple operation. This project's goal
+ is, understanding how to parse the grammer such that:
+
+1. the language has both left/right-associative operators
+2. the language has the order of operators
+
+There I think simple languages that has characterisities above. Concretely,
+ this code parses and evaluates simple arithmatic operations, that is:
+
+- left associative operations
+    - addition `+` and substruction `-`, low priority operations
+    - multiply `*` and division `/`, high priority operations
+- right associative operations
+    - exponential `**`,
+
+and integer operands (interpreter may prints float number as result, but user
+can not input float but integer).
 """
 
 def tokenize(s):
